9884d592525e9985fec6a1bbe382aa6982d011ab
setha.va@sompom.com <setha.va@sompom.com>
2023-01-22 16:56:16 +0700

    Bug: (login) can wrong status value

--- src/helper/validate.js
+++ src/helper/validate.js
@@ -4,12 +4,12 @@ import { authenticate } from "./helper";
 
 // Validate login screen
 export async function usernameValidate(values){
-    const error = usernameValidate({}, values);
+    const error = usernameVerify({}, values);
 
     if(values.username){
         // check user exist or not
-        const status = await authenticate(values.username);
-
+        const {status} = await authenticate(values.username);
+        
         if(status !== 200){
             error.exist = toast.error("User doesn't exist...!")
         }
